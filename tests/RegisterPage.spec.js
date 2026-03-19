ce3735c53768e28d3245eb38366f644550a600b6
HasaliM23 <hasali2000pm@gmail.com>
2026-03-19 16:44:28 -0700

    Update

--- tests/RegisterPage.spec.js
+++ tests/RegisterPage.spec.js
@@ -15,7 +15,7 @@ test("Verify that the user can register successfully" , async({page})=>{
     });
     //npx playwright test RegisterPage.spec.js --project chromium --headed 
 
-test.only("Verify password Mismatch error" , async({page})=>{
+test("Verify password Mismatch error" , async({page})=>{
 
     await page.goto("https://practice.expandtesting.com/register");
 
@@ -27,7 +27,22 @@ test.only("Verify password Mismatch error" , async({page})=>{
     await page.waitForTimeout(3000);
 
 
+});
 
+test.only("Verify that the Password character limit is enforced" , async({page})=>{
 
+    await page.goto("https://practice.expandtesting.com/register");
+
+    await page.locator("(//input[@id='username'])[1]").fill("Harry");
+
+    await page.locator("(//input[@id='password'])[1]").fill("Harry123456789012345678900000000000000");
+
+    await page.locator("(//input[@id='confirmPassword'])[1]").fill("Harry123456789012345678900000000000000");
+
+    await page.locator("(//button[normalize-space()='Register'])[1]").click();
+
+     await page.waitForTimeout(3000);
 
 });
+
+
