5f9bdaae96e786e2d3dd51af8f5bb6b1a837803d
HasaliM23 <hasali2000pm@gmail.com>
2026-03-19 17:10:18 -0700

    Update

--- tests/LoginPage.spec.js
+++ tests/LoginPage.spec.js
@@ -16,3 +16,28 @@ test ("Verify that the user can login successfully" , async({page})=>{
 
 
 });
+
+test("Verify that the user cannot login with invalid credentials" , async({page})=>{
+
+    await page.goto("https://practice.expandtesting.com/login");
+
+    await page.fill("(//input[@id='username'])[1]","practice");
+
+    await page.fill("(//input[@id='password'])[1]" , "SuperSecretPassword");
+
+    await page.locator("(//button[normalize-space()='Login'])[1]").click();
+
+    await page.waitForTimeout(3000);
+
+
+});
+
+test.only("Verify that the user cannot login with empty fields" , async({page})=>{
+
+
+    await page.goto("https://practice.expandtesting.com/login");
+    await page.locator("(//button[normalize-space()='Login'])[1]").click();
+
+    await page.waitForTimeout(3000);
+
+});
