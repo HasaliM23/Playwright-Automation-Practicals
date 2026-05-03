82e22f8bc58bb9363537d286694d1765e791e520
HasaliM23 <hasali2000pm@gmail.com>
2026-05-02 19:37:13 -0700

    update

--- tests/PracticeFormOtherTestcases.spec.js
+++ tests/PracticeFormOtherTestcases.spec.js
@@ -7,10 +7,53 @@ test("verify leave mandotory feild", async({page})=>{
     await page.locator("//input[@id='submit-id-submit']").click();
 
 
-    await page.waitForTimeout(9000);
+    await page.waitForTimeout(3000);
+
+    
+});
+
+    test("verify that invalid email format is not accepted", async({page})=>{
+
+        await page.goto("https://www.qa-practice.com/forms/practice-form");
+
+        await page.locator("//input[@id='firstName']").fill("Harry");
+
+        await page.locator("//input[@id='lastName']").fill("Potter");
+
+        await page.locator("//input[@id='gender_0']").check();
+
+    await page.locator("//input[@id='userNumber']").fill("1234567890");
+
+        await page.locator("//input[@id='userEmail']").fill("harrypotterexample.com");
+        await page.locator("//input[@id='submit-id-submit']").click();
+        await page.waitForTimeout(3000);
+
+    });
+
+    test.only("verify that invalid phone number format is not accepted", async({page})=>{
+
+        await page.goto("https://www.qa-practice.com/forms/practice-form");
+
+        await page.locator("//input[@id='firstName']").fill("Harry");
+        await page.locator("//input[@id='lastName']").fill("Potter");
+        await page.locator("//input[@id='userEmail']").fill("harry.potter@example.com");
+        await page.locator("//input[@id='gender_0']").check();
+
+        await page.locator("//input[@id='userNumber']").fill("12345678");
+
+        await page.locator("//input[@id='submit-id-submit']").click();
+        await page.waitForTimeout(3000);
+
+      
+        
+
+
+    });
+
+
+
+
 
     //npx playwright test PracticeFormOtherTestcases.spec.js --project chromium --headed
 
 
-
-});
